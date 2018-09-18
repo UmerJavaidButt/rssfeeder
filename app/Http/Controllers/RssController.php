f8e1b3e114690078f1631d5404610a121581ff61
omerjavbutt <omerjavbutt@gmail.com>
2018-09-18 12:32:28 -0700

    first commit

---
+++ app/Http/Controllers/RssController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use FeedReader;
+use Feeds;
+class RssController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    //
+    function index(){
+    	return view('home.feeds');
+    }
+
+    function feed(Request $request){
+        $extention = '/collections/all.atom';
+        //$https = 'https://';
+    	$url = $request->get('url');
+    	$feed = Feeds::make($url.$extention);
+
+        $data = array(
+            'title'     => $feed->get_title(),
+            'permalink' => $feed->get_permalink(),
+            'items'     => $feed->get_items(),
+        );
+
+        return view('home.feeds', compact('data'));
+        
+    }
+}
